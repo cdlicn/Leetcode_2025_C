6e70ab4109c7c9c63b83f9b9c63816e6ba250526
Liyuanzhe2001 <619356147@qq.com>
2025-01-01 21:30:16 +0800

    2024-01-01

---
+++ main_3280.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+string bin(int val) {
+    string res = "";
+    while (val) {
+        res = (val & 1 ? "1" : "0") + res;
+        val >>= 1;
+    }
+    return res;
+}
+
+string convertDateToBinary(string date) {
+    return bin(stoi(date.substr(0, 4))) + "-" + bin(stoi(date.substr(5, 2))) + "-" + bin(stoi(date.substr(8, 2)));
+}
