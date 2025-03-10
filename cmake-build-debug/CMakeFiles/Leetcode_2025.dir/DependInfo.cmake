2b35c675e178b5f6d3a4521979ecbd6ecb7173f5
Liyuanzhe2001 <619356147@qq.com>
2025-03-10 21:58:28 +0800

    2025-03-10

--- cmake-build-debug/CMakeFiles/Leetcode_2025.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Leetcode_2025.dir/DependInfo.cmake
@@ -37,6 +37,7 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/project/C++/Leetcode_2025/main_2241.cpp" "CMakeFiles/Leetcode_2025.dir/main_2241.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2241.cpp.obj.d"
   "C:/project/C++/Leetcode_2025/main_2264.cpp" "CMakeFiles/Leetcode_2025.dir/main_2264.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2264.cpp.obj.d"
   "C:/project/C++/Leetcode_2025/main_2266.cpp" "CMakeFiles/Leetcode_2025.dir/main_2266.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2266.cpp.obj.d"
+  "C:/project/C++/Leetcode_2025/main_2269.cpp" "CMakeFiles/Leetcode_2025.dir/main_2269.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2269.cpp.obj.d"
   "C:/project/C++/Leetcode_2025/main_2270.cpp" "CMakeFiles/Leetcode_2025.dir/main_2270.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2270.cpp.obj.d"
   "C:/project/C++/Leetcode_2025/main_2274.cpp" "CMakeFiles/Leetcode_2025.dir/main_2274.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2274.cpp.obj.d"
   "C:/project/C++/Leetcode_2025/main_2275.cpp" "CMakeFiles/Leetcode_2025.dir/main_2275.cpp.obj" "gcc" "CMakeFiles/Leetcode_2025.dir/main_2275.cpp.obj.d"
--- main_2269.cpp
+++ main_2269.cpp
@@ -1,3 +1,19 @@
-//
-// Created by LiYuanzhe on 2025/3/10.
-//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int divisorSubstrings(int num, int k) {
+    int n = 0, bn = num;
+    for (int i = 0; i < k; ++i) {
+        n += pow(10, i) * (num % 10);
+        num /= 10;
+    }
+    int res = n != 0 && bn % n == 0;
+    while (num) {
+        n /= 10;
+        n += pow(10, k - 1) * (num % 10);
+        num /= 10;
+        res += n != 0 && bn % n == 0;
+    }
+    return res;
+}
