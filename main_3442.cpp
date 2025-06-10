3be30254ddb51c09036f982ee8396987f6674bb0
Liyuanzhe2001 <619356147@qq.com>
2025-06-10 22:01:00 +0800

    2025-06-10

--- main_3442.cpp
+++ main_3442.cpp
@@ -1,3 +1,19 @@
-//
-// Created by LiYuanzhe on 2025/6/10.
-//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int maxDifference(string s) {
+    vector<int> cnt(26, 0);
+    for (const auto &c: s) {
+        cnt[c - 'a']++;
+    }
+    int a1 = 0, a2 = s.size();
+    for (const auto &a: cnt) {
+        if (a % 2) {
+            a1 = max(a1, a);
+        } else if (a) {
+            a2 = min(a2, a);
+        }
+    }
+    return a1 - a2;
+}
