6bcbd322dd206ba3ca378a1013855ac089c45ae9
cdlicn <619356147@qq.com>
2025-07-26 17:08:48 +0800

    2025-07-26

---
+++ main_3480.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// TODO 3480. 删除一个冲突对后最大子数组数目
+long long maxSubarrays(int n, vector<vector<int>> &conflictingPairs) {
+    vector<vector<int>> groups(n + 1);
+    for (auto &p: conflictingPairs) {
+        int a = p[0], b = p[1];
+        if (a > b) {
+            swap(a, b);
+        }
+        groups[a].push_back(b);
+    }
+
+    long long ans = 0;
+    vector<long long> extra(n + 2);
+    vector<int> b = {n + 1, n + 1};
+
+    for (int i = n; i > 0; i--) {
+        // 维护最小 b 和次小 b
+        b.insert(b.end(), groups[i].begin(), groups[i].end());
+        ranges::sort(b);
+        b.resize(2);
+
+        ans += b[0] - i;
+        extra[b[0]] += b[1] - b[0];
+    }
+
+    return ans + ranges::max(extra);
+}
