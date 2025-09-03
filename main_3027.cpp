dde0478717fc38b9d04627edab56056504b51492
cdlicn <619356147@qq.com>
2025-09-03 21:44:19 +0800

    2025-09-03

---
+++ main_3027.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+static bool cmp(vector<int> a, vector<int> b) {
+    if (a[0] < b[0]) {
+        return true;
+    }
+    if (a[0] > b[0]) {
+        return false;
+    }
+    return a[1] > b[1];
+}
+
+int numberOfPairs(vector<vector<int>> &points) {
+    sort(points.begin(), points.end(), cmp);
+    int res = 0, n = points.size();
+    for (int i = 0; i < n; ++i) {
+        int y = points[i][1], max_y = INT_MIN;
+        for (int j = i + 1; j < n && max_y <= y; ++j) {
+            if (points[j][1] <= y && points[j][1] > max_y) {
+                max_y = points[j][1];
+                res++;
+            }
+        }
+    }
+    return res;
+}
