d885cf27f048f50a83a377cfcf6ece54d40b8685
cdlicn <619356147@qq.com>
2025-09-30 09:40:07 +0800

    2025-09-30

---
+++ main_2221.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int triangularSum(vector<int> &nums) {
+    int n = nums.size();
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            nums[j] = (nums[j] + nums[j + 1]) % 10;
+        }
+    }
+    return nums[0];
+}
