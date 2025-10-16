f6593b7c6b86cda853c3513f7d30cbeb47d69ccb
cdlicn <619356147@qq.com>
2025-10-16 23:01:24 +0800

    2025-10-16

---
+++ main_2598.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int findSmallestInteger(vector<int> &nums, int m) {
+    unordered_map<int, int> cnt;
+    for (int x: nums) {
+        cnt[(x % m + m) % m]++;
+    }
+
+    int mex = 0;
+    while (cnt[mex % m]-- > 0) {
+        mex++;
+    }
+    return mex;
+}
