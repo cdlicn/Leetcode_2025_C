4f34bdad67433c289bbd650247b5cb76f2c99a80
cdlicn <619356147@qq.com>
2025-10-18 21:36:14 +0800

    2025-10-18

---
+++ main_3397.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int maxDistinctElements(vector<int> &nums, int k) {
+    ranges::sort(nums);
+    int ans = 0;
+    int pre = INT_MIN;
+    for (int x: nums) {
+        x = min(max(x - k, pre + 1), x + k);
+        if (x > pre) {
+            ans++;
+            pre = x;
+        }
+    }
+    return ans;
+}
